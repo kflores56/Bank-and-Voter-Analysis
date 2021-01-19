f5acbe70c5cf84fef5869c28e95a58a5a8dd01c1
Kristin Flores <kristin@Joan-Steppenbeck.local>
2021-01-18 20:48:57 -0600

    added greatest increase and decrease

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,37 +5,51 @@ import csv
 #Set CSV path
 budget_csv = os.path.join('PyBank', 'Resources', 'budget_data.csv')
 
-#Set zero
+#Set variables 
 total_months = 0
-net_amount = 0
+month = []
+amount = []
 
-#Define Variables
-def financial_analysis(budget_csv):
-
-    total_months = str(budget_csv[0])
-    amount = int(budget_csv[1])
-
-#Open and read CSV
-with open(budget_csv) as csvfile:
+#open and read CSV
+with open(budget_csv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
 
-    month = []
-    amount = []
-
     for row in csvreader:
 
+        #Total number of months by counting number of lines
+        total_months += 1
+        
+        #append to lists
         month.append(row[0])
-        amount.append(int(row[1]))
+        amount.append(float(row[1]))      
 
-        #Total number of months
-        total_months = len(list(csvreader)) + 1
+net_amount = amount[0]
+monthly_change = 0
+high_profit = 0
+low_profit = 0
 
-        #Net profit amount
-        net_amount = sum(amount)
+for n in range (1, total_months):
 
+    #Net profit amount
+    net_amount += amount[n]
 
-    print(f"Total Months: {total_months}")
-    print(f"Total: {net_amount}")
-    
+    #Find change in monthly amounts
+    current_change = amount[n] - amount[n-1]
+    monthly_change += current_change
 
+    #greatest increase and decrease
+    if current_change > high_profit:
+        high_profit = current_change
+        high_profit_date = month [n]
+    elif current_change < low_profit:
+        low_profit = current_change
+        low_profit_date = month[n]
+
+print(f"Financial Analysis")
+print(f"-----------------------------")
+print(f"Total Months: {total_months}")
+print(f"Total Profit: $ {net_amount}")
+print(f"Average Change: $ {monthly_change}")
+print(f"Greatest Increase in Profits: {high_profit_date} $ {high_profit}")
+print(f"Greastest Decrease in Profits: {low_profit_date} $ {low_profit}")
---
+++ PyBank/mainloop.py
@@ -0,0 +1,54 @@
+#Import OS & CSV
+import os
+import csv
+
+#Set CSV path
+budget_csv = os.path.join('PyBank', 'Resources', 'budget_data.csv')
+
+#Set zero
+total_months = 0
+net_amount = 0
+average_change = 0
+current_profit = 0
+high_profit = 0
+high_profit_date = ""
+low_profit = 0
+low_profit_date = ""
+
+
+#open and read CSV
+with open(budget_csv, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+
+    for row in csvreader:
+
+        #Total number of months by counting number of lines
+        total_months += 1
+
+        #Net profit amount
+        net_amount += int(row[1])
+
+        #Find change in monthly amounts
+        current_profit = int(row[1])
+        if(current_profit >=0):
+            if(current_profit > high_profit):
+                high_profit = current_profit
+                high_profit_date = str(row[0])
+        elif(current_profit<0):
+            if(current_profit < low_profit):
+                low_profit = current_profit
+                low_profit_date = str(row[0])
+
+#find average
+average_change = round(net_amount / total_months, 2)
+
+print(f"Financial Analysis")
+print(f"-----------------------------")
+print(f"Total Months: {total_months}")
+print(f"Total Profit: $ {net_amount}")
+print(f"Average Change: $ {average_change}")
+print(f"Greatest Increase in Profit: {high_profit_date} $ {high_profit}")
+print(f"Greatest Decrease in Profit: {low_profit_date} $ {low_profit}")
+    
+
